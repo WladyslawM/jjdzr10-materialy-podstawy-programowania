4a1ba09a72eb25fcd4e7de0eec5cea11f0ff7901
WladyslawM <uladzislaumikhno@gmail.com>
2023-02-04 20:30:55 +0100

    clear main

--- src/main/java/com/infoshareacademy/Main.java
+++ src/main/java/com/infoshareacademy/Main.java
@@ -4,24 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            picUpNamber();
+
 
     }
 
-    private static void picUpNamber() {
-
-        Scanner scanner = new Scanner(System.in);
-        while ()
-        try {
-            System.out.println("Podaj liczbe int: ");
-            double result1 = scanner.nextInt();
-            System.out.println("Podaj liczbe double: ");
-            double result2 = scanner.nextDouble();
-            System.out.println(result1);
-            System.out.println(result2);
-        } catch (Exception e) {
-            System.out.println("blendny wpis");
-        }
-
-    }
 }
